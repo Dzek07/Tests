c99ee701c19333998add05eda2a394284b79c9a5
Dzek <dzaek07@gmail.com>
2022-10-02 21:41:33 +0300

    Создали пустой одномерный массив и вывели его

---
+++ Test_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Количество элиментов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+
+
+Console.Write(string.Join(", ", array));
