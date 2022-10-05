dcdec959972d64e77be4aa91595a6811f54d300d
Dzek <dzaek07@gmail.com>
2022-10-05 11:18:21 +0300

    Добавил функцию для заполнения результирующего массива как требует задание

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -20,6 +20,19 @@ int Elements_less_than_three_characters(string[] array)
     }
     return counter;
 }
+void Writing_to_a_new_array(string[] array, string[] resoult_array)
+{
+    int size = array.Length;
+    int j = 0;
+    for(int index = 0; index < size; index++)
+    {      
+        if (array[index].Length < 4)
+        {   
+            resoult_array[j] = array[index];
+            j++;
+        }
+    }
+}
 
 Console.Write("Количество элиментов в массиве: ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -32,3 +45,5 @@ Console.Write(string.Join(", ", array));
 Console.WriteLine();
 int size_resoult_array = Elements_less_than_three_characters(array);
 string[] resoult_array = new string[size_resoult_array];
+Writing_to_a_new_array(array, resoult_array);
+Console.Write(string.Join(", ", resoult_array));
