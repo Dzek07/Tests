a398e826c8dc48654d325d977d09346d915c0d4c
Dzek <dzaek07@gmail.com>
2022-10-05 11:13:54 +0300

    Создал массив и зеполнил его через функцию значениями с клавиатурыи вывел его на экран

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -1,7 +1,22 @@
-﻿Console.WriteLine("Количество элиментов в массиве: ");
+﻿void ArrayFilling(string[] array)
+{
+    int size = array.Length;
+
+    for(int index = 0; index < size; index++)
+    {
+        Console.Write("Введите элимент масссива: ");
+        array[index] = Console.ReadLine();
+    }
+
+}
+
+
+Console.Write("Количество элиментов в массиве: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
 string[] array = new string[size];
 
 
+ArrayFilling(array);
 Console.Write(string.Join(", ", array));
+Console.WriteLine();
