ee071f553e3229f527e9fe35d45399fc31423b8b
Dzek <dzaek07@gmail.com>
2022-10-05 11:16:20 +0300

    Добавил новый массив для результата и определил его длинну через функцию

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -9,7 +9,17 @@
     }
 
 }
-
+int Elements_less_than_three_characters(string[] array)
+{
+    int size = array.Length;
+    int counter = 0;
+    for(int index = 0; index < size; index++)
+    {          
+        if (array[index].Length < 4)
+            counter++;
+    }
+    return counter;
+}
 
 Console.Write("Количество элиментов в массиве: ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -20,3 +30,5 @@ string[] array = new string[size];
 ArrayFilling(array);
 Console.Write(string.Join(", ", array));
 Console.WriteLine();
+int size_resoult_array = Elements_less_than_three_characters(array);
+string[] resoult_array = new string[size_resoult_array];
